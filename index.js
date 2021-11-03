b94f5244ad961ff6d34108a21ebb67fb7896ab0a
lim-jiahao <lim.jiahaoo@gmail.com>
2021-11-03 15:39:57 +0800

    listed all sightings in index / route

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import { read } from './jsonFileStorage.js';
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 const FILENAME = 'data.json';
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+const getAllSightings = (req, res) => {
+  read(FILENAME, (err, data) => {
+    if (err) {
+      console.error('Read error', err);
+      res.status(500).send(err);
+      return;
+    }
+
+    res.status(200).render('index', data);
+  });
+};
+
+app.get('/', getAllSightings);
 
 app.listen(3004);
